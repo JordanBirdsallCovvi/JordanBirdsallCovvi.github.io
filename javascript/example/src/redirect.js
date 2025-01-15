10ed8a8880bc1c461572ee2d4cec6e5548116120
Jordan Birdsall <jordan.birdsall@covvi.com>
2025-01-15 14:34:40 +0000

    Print 'location.href'.

--- javascript/example/src/redirect.js
+++ javascript/example/src/redirect.js
@@ -1,5 +1,6 @@
 // redirect from example to example/bundle for demo
 // link backward compatibility only on github-hosted demo
+console.log(location.href);
 const url = new URL(location.href);
 const tokens = url.pathname.split(/[\\/]/g);
 const filename = tokens.pop();
